e2d8d285f0c529389457e7d8c6927e5d072c9950
AlekseiAnikeev <info.anikeev@gmail.com>
2022-11-02 22:07:06 +0300

    removed magic number

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
-
 public class Main {
+    public static final int PORT = 8989;
     public static void main(String[] args) throws Exception {
 //        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
 //        System.out.println(new Gson().toJson(engine.search("бизнес")));
-        SearchServer server = new SearchServer(8989);
+        SearchServer server = new SearchServer(PORT);
         server.start();
     }
 }
